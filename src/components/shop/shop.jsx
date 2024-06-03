47c8fab938529c8f078ccb5d79c05d693fd497fa
Caio <cs1919328@gmail.com>
2024-06-02 22:46:33 -0300

    firebase 02/06/2024

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -7,35 +7,6 @@ import { classItem } from './Item';
 import { formatNumberInCurrency } from '../cart/cart-components';
 
 export default function Itens() {
-  const itens = [
-    'https://i.postimg.cc/qq1d8Gvb/arcanine-h.png',
-    'https://i.postimg.cc/dQ6vPJj2/arcanine-s.png',
-    'https://i.postimg.cc/hjbnGnWq/blaziken-n.png',
-    'https://i.postimg.cc/3N8T4qY0/camerupt-n.png',
-    'https://i.postimg.cc/8PCGTxZq/chandelure-n.png',
-    'https://i.postimg.cc/c45SCTj6/charizard-c.png',
-    'https://i.postimg.cc/SxBb7Rmh/charizard-h.png',
-    'https://i.postimg.cc/26mpMtq9/cindaquil-h.png',
-    'https://i.postimg.cc/76VvkN0H/darmanitan-s.png',
-    'https://i.postimg.cc/1X5k5Nrp/darumaka-su.png',
-    'https://i.postimg.cc/rsnvdFf7/delphox-h.png',
-    'https://i.postimg.cc/PfSGV2dK/emboar-su.png',
-    'https://i.postimg.cc/xTZwn8Jz/flareon-n.png',
-    'https://i.postimg.cc/bw2KJPkF/houndoom-n.png',
-    'https://i.postimg.cc/k5QzQyjQ/infernape-p.png',
-    'https://i.postimg.cc/D0MMvpZT/larvesta-h.png',
-    'https://i.postimg.cc/h4yw1v05/magmortar-n.png',
-    'https://i.postimg.cc/HsFhFNR0/ninetails.png',
-    'https://i.postimg.cc/136jSH1k/ninetails-n.png',
-    'https://i.postimg.cc/cJBkgLHT/ninetails-p.png',
-    'https://i.postimg.cc/85kKhgfG/ninetales-v.png',
-    'https://i.postimg.cc/tTrrkvFx/ponyta-h.png',
-    'https://i.postimg.cc/tJYBMdzm/ponyta-s.png',
-    'https://i.postimg.cc/NFSdqyJG/slugma-1.png',
-    'https://i.postimg.cc/BZVY1ySW/torkoal-h.png',
-    'https://i.postimg.cc/Prv6b4JC/volcarona-p.png'
-  ];
-
   const [name, setName] = useState([]);
   const [data, setData] = useState();
 
